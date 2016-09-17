aa260a3b50a410ac46f70ffbed927ceba20f4910
toshiemon18 <st12318@gmail.com>
2016-09-17 18:17:00 +0900

    Add fetch_classchange method

--- api/classchange_api.rb
+++ api/classchange_api.rb
@@ -7,19 +7,29 @@ require "open-uri"
 require "nokogiri"
 
 module TNCTClassChangeLINEBOT
-  class ChassChangeAPI
-    def initialize(url, xpath)
-      @url = url
+  class ClassChangeAPI
+    def initialize(api_config)
+      config = api_config
+      # @url = config["url"]
+      # @xpath = config["xpath"]
+      @url = "http://jyugyou.tomakomai-ct.ac.jp/jyugyou.php?date=2016.7.27"
+      @xpath = "//table[@width=\"70%\"]/tr[@height=\"35\"]"
     end
 
     def fetch_classchange
       charset = nil
-      html = open(url) do |stream|
+      html = open(@url) do |stream|
         charset = stream.charset
         stream.read
       end
 
-      page = Nokogiri::HTML.parse(html, nil, charset)
+      doc = Nokogiri::HTML.parse(html, nil, charset)
+      puts doc.xpath(@xpath).each {|e| puts e}
+      # doc.xpath(@xpath).each do |elem|
+      # end
     end
   end
 end
+
+mod = TNCTClassChangeLINEBOT::ClassChangeAPI.new(nil)
+mod.fetch_classchange
