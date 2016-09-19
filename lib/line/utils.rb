22f5bc31f19e01eefe9c55eff06a1d5644084e3d
toshiemon18 <st12318@gmail.com>
2016-09-19 19:04:59 +0900

    Remove EasyLineAPI module

--- lib/line/utils.rb
+++
@@ -1,9 +0,0 @@
-# codinf : utf-8
-
-module TmNCTClassChangeLINEBOT
-  module EasyLineAPI
-    module Utils
-      class InvalidCredentialError < Error; end
-    end
-  end
-end
--- lib/line_easy_api.rb
+++ lib/line_easy_api.rb
@@ -9,38 +9,5 @@ require "uri"
 module TmNCTClassChangeLINEBOT
   module EasyLineAPI
 
-    class InvalidCredentialError < Error; end
-
-    class Client
-      attr_accessor :channel_id, :channel_secret, :mid, :endpoint
-      DEFAULT_ENDPOINT = "https://trialbot-api.line.me/v1"
-
-      def initialize(options={})
-        options.each {|k, v| instance_varable_set("#{k}", v) }
-        yield self if block_given?
-
-        @http_client = TmNCTClassChangeLINEBOT::EasyLineAPI::HttpConnector.new
-      end
-
-      def send_message()
-        unless valid_credentials?
-          raise TmNCTClassChangeLINEBOT::EasyLineAPI::InvalidCredentialError, "Invalidates credentials"
-        end
-      end
-
-      private
-      def credentials
-        {
-          "X-Line-ChannelID"             => channel_id,
-          "X-Line-ChannelSecret"         => channel_secret,
-          "X-Line-Trusted-User-With-ACL" => mid
-        }
-      end
-
-      def valid_credentials?
-        credentials.values.all?
-      end
-
-    end
   end
 end
