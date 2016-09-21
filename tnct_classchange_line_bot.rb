8cacca9770951712f5c40acaa16474409dc5d21a
toshiemon18 <st12318@gmail.com>
2016-09-22 00:37:59 +0900

    Dev line bot (#4)
    * Develop Line BOT
    
    * Disable json
    
    * Add bot file
    
    * Abort...
    
    * Add set_date

--- tnct_classchange_line_bot.rb
+++ tnct_classchange_line_bot.rb
@@ -9,11 +9,44 @@ module TmNCTClassChangeLINEBOT
   class BOT < Sinatra::Base
     attr_accessor :classchange
 
+<<<<<<< HEAD
+    def load_helper
+      @helper = TmNCTClassChangeLINEBOT::Helper.new
+    end
+
+    def fetch_classchange
+      self.classchange = @helper.classchange_client.run
+    end
+
+    post "/callback" do
+      load_helper
+
+      current_time = @helper.fetch_current_time
+
+      params = JSON.parse(request.body.read)
+      params[:result].each do |msg|
+        next if msg["content"]["contentType"] != 1
+        message = msg["content"]["text"]
+        from_user_id = msg["content"]["from"]
+
+        key = translate_to_key(message)
+        if !key
+          @helper.line_client.send(from_user_id, "そのクラスは存在しませんよ！")
+        elsif !@helper.has_classchange?(key, classchange)
+          @helper.line_client.send(from_user_id, "授業変更はありません！")
+        else
+          @helper.line_client.send(from_user_id,
+            ""
+          )
+        end
+      end
+=======
     def initialize
       @helper = TmNCTClassChangeLINEBOT::Helper.new
     end
 
     post "/callback" do
+>>>>>>> a50c891b30d2d08bc51b8a3ae1666d02bef1d86c
     end
   end
 end
